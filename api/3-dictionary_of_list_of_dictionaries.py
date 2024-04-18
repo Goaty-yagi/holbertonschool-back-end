fe0aad625e385e782a576d46afd2e13ae59a2e20
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-18 12:01:19 +1000

    fix: remove extra appending

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -10,7 +10,7 @@ import requests
 def export_all_todo():
     base_url = "https://jsonplaceholder.typicode.com"
 
-    todo_url = f"/todos"
+    todo_url = "/todos"
     todo_response = requests.get(base_url + todo_url)
 
     if todo_response.status_code == 200:
@@ -18,42 +18,36 @@ def export_all_todo():
     else:
         return 'Error fetching data: {}'.format(todo_response.status_code)
 
-    with open("todo_all_employees.json", 'w') as file:
-        sorted_todos = sorted(all_todos, key=lambda x: x['userId'])
-        init_id = sorted_todos[0]['id']
-        user_url = f"/users/{init_id}"
-        user_response = requests.get(base_url + user_url)
-        if user_response.status_code == 200:
-            user = user_response.json()
-        else:
-            return 'Error fetching data: {}'.format(user_response.status_code)
+    sorted_todos = sorted(all_todos, key=lambda x: x['userId'])
+    init_id = sorted_todos[0]['id']
+    user_url = f"/users/{init_id}"
+    user_response = requests.get(base_url + user_url)
+    if user_response.status_code == 200:
+        user = user_response.json()
+    else:
+        return 'Error fetching data: {}'.format(user_response.status_code)
 
-        each_list = []
-        store = {}
-        for todo in sorted_todos:
-            if init_id != todo['userId']:
-                store[init_id] = each_list
-                each_list = []
-                init_id = todo['userId']
-                user_url = f"/users/{init_id}"
-                user_response = requests.get(base_url + user_url)
-                if user_response.status_code == 200:
-                    user = user_response.json()
-                else:
-                    return 'Error fetching data: \
-                        {}'.format(user_response.status_code)
-            each_list.append({
-                'username': user["username"],
-                'task': todo["title"],
-                'completed': todo["completed"]
-            })
-
+    each_list = []
+    store = {}
+    for todo in sorted_todos:
+        if init_id != todo['userId']:
+            store[init_id] = each_list
+            each_list = []
+            init_id = todo['userId']
+            user_url = f"/users/{init_id}"
+            user_response = requests.get(base_url + user_url)
+            if user_response.status_code == 200:
+                user = user_response.json()
+            else:
+                return 'Error fetching data: \
+                    {}'.format(user_response.status_code)
         each_list.append({
             'username': user["username"],
             'task': todo["title"],
             'completed': todo["completed"]
         })
-        store[init_id] = each_list
+    store[init_id] = each_list
+    with open("todo_all_employees.json", 'w') as file:
         json.dump(store, file)
 
 
